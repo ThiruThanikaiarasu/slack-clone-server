2dd8ef4f77b4f8c1e2d45bb7b8bc69dda319f2f0
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-11-19 15:07:00 +0530

    Add. Included login and signup routers.

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express()
 const PORT = 3500
 
 const mongoose = require('mongoose')
+const cors = require('cors')
+
+const loginRoute = require('./routes/loginRoute')
+const signupRoute = require('./routes/signupRoute')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
 app.get('/', (request, response) => {
     response.status(200).json({message : "Welcome to Slack Clone"})
@@ -13,5 +21,8 @@ mongoose.connect(process.env.DB_URL)
 const db = mongoose.connection
 db.on('error', (errorMessage) => console.log(errorMessage))
 db.once('open', () => console.log('Connected to db successfully')) 
+
+app.use('/api/v1/login', loginRoute)
+app.use('/api/v1/signup', signupRoute)
 
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}/api/v1`))
