b4c3d45a24e297e176437f83f72fb4bb9db1ce89
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-11-19 14:11:10 +0530

    Established mongodb Atlas Connection.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = 3500
 
+const mongoose = require('mongoose')
+
 app.get('/', (request, response) => {
     response.status(200).json({message : "Welcome to Slack Clone"})
 })
+
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+db.on('error', (errorMessage) => console.log(errorMessage))
+db.once('open', () => console.log('Connected to db successfully')) 
 
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}/api/v1`))
